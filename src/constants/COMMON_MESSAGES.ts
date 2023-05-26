a604df2021da1ca0616e508968c5da44c14b0cbf
kazu-pl <91014542+kazu-pl@users.noreply.github.com>
2023-05-26 18:11:59 +0200

    create actions for user: updateUserPassword, updateUserData, getUserData, removeUser (#3)

--- src/constants/COMMON_MESSAGES.ts
+++ src/constants/COMMON_MESSAGES.ts
@@ -1,5 +1,7 @@
 const COMMON_MESSAGES = {
   AN_ERROR_OCCURED: "Wystąpił nieoczekiwany błąd.",
+  NOT_FOUND: "Nie znaleziono zasobu",
+  NOT_FOUND_FN: (notFoundItem: string) => `Nie znaleziono ${notFoundItem}`,
 };
 
 export default COMMON_MESSAGES;
--- src/features/auth/User.model.ts
+++ src/features/auth/User.model.ts
@@ -6,6 +6,11 @@ export interface User extends Document {
     name: string;
     surname: string;
     email: string;
+    phone: number;
+    street: string;
+    streetNumber: string;
+    postalCode: string;
+    city: string;
   };
 }
 
@@ -13,6 +18,17 @@ const UserSchema: Schema = new Schema(
   {
     password: { type: String, required: true },
     data: {
+      // type: {
+      //   name: { type: String, required: true },
+      //   surname: { type: String, required: true },
+      //   email: { type: String, required: true },
+      //   phone: { type: Number, required: true },
+      //   street: { type: String, required: true },
+      //   streetNumber: { type: String, required: true },
+      //   postalCode: { type: String, required: true },
+      //   city: { type: String, required: true },
+      //   _id: { type: Object, required: true },
+      // }, // for some reason if I leave this object then updateOne won't work
       type: Object,
       required: true,
     },
@@ -22,6 +38,6 @@ const UserSchema: Schema = new Schema(
   }
 );
 
-const userModel = mongoose.model<User>("User", UserSchema);
+const UserModel = mongoose.model<User>("User", UserSchema);
 
-export default userModel;
+export default UserModel;
