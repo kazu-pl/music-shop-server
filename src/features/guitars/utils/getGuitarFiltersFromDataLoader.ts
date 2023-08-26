34e7c8d705a025e6f191907e375e8a070a1e6c04
kazu-pl <91014542+kazu-pl@users.noreply.github.com>
2023-08-26 13:57:20 +0200

    fix hardcoded resolver that returned availability filters (#22)

--- src/features/guitars/utils/getGuitarFiltersFromDataLoader.ts
+++ src/features/guitars/utils/getGuitarFiltersFromDataLoader.ts
@@ -1,15 +1,15 @@
-import { Loaders } from "loaders";
+import { LoadersKeys } from "loaders/loaders";
 import mongoose from "mongoose";
 import { GuitarWithDataLoader } from "types/graphql.types";
 import { Context } from "vm";
 
 const getGuitarFiltersFromDataLoader = async (
-  parent: GuitarWithDataLoader,
+  parent: GuitarWithDataLoader, // parent is gonna be an object with guitar which has fields like `availability: 43654gb5h`
   context: Context,
-  dataLoaderKey: keyof Loaders
+  dataLoaderKey: keyof LoadersKeys
 ) => {
   const id = (
-    parent.availability as unknown as mongoose.Types.ObjectId
+    parent[dataLoaderKey] as unknown as mongoose.Types.ObjectId
   ).toHexString(); // get _id string value instead of New ObjectId("...")
 
   return await context.loaders[dataLoaderKey].load(id);
--- src/loaders/loaders.ts
+++ src/loaders/loaders.ts
@@ -1,27 +1,40 @@
 // import availabilityLoader from "./availabilityLoader";
 
 import GuitarFilterModel from "features/guitars/filters/GuitarFilter.model";
-import { GuitarFilter } from "types/graphql.types";
+import { GuitarFilter, GuitarWithDataLoader } from "types/graphql.types";
 import commonLoader from "./commonLoader";
+import DataLoader from "dataloader";
+
+export type LoadersKeys = Pick<
+  GuitarWithDataLoader,
+  | "availability"
+  | "bodyWood"
+  | "bridge"
+  | "fingerboardWood"
+  | "guitarType"
+  | "pickupsSet"
+  | "producer"
+  | "shape"
+>;
 
 /**
  * `loaders` is an object whose keys are models from graphql schema. Each of that model has another keys which are loaders for the concrete field of the model
  */
-const loaders = {
-  availabilityLoader: commonLoader<GuitarFilter>(
-    GuitarFilterModel,
-    "availabilities"
-  ),
-  bodyWoodLoader: commonLoader<GuitarFilter>(GuitarFilterModel, "bodyWood"),
-  bridgeLoader: commonLoader<GuitarFilter>(GuitarFilterModel, "bridge"),
-  fingerboardWoodLoader: commonLoader<GuitarFilter>(
+const loaders: Record<
+  keyof LoadersKeys,
+  DataLoader<string, GuitarFilter, string>
+> = {
+  availability: commonLoader<GuitarFilter>(GuitarFilterModel, "availabilities"),
+  bodyWood: commonLoader<GuitarFilter>(GuitarFilterModel, "bodyWood"),
+  bridge: commonLoader<GuitarFilter>(GuitarFilterModel, "bridge"),
+  fingerboardWood: commonLoader<GuitarFilter>(
     GuitarFilterModel,
     "fingerboardWood"
   ),
-  guitarTypeLoader: commonLoader<GuitarFilter>(GuitarFilterModel, "guitarType"),
-  pickupsSetLoader: commonLoader<GuitarFilter>(GuitarFilterModel, "pickupsSet"),
-  producerLoader: commonLoader<GuitarFilter>(GuitarFilterModel, "producer"),
-  shapeLoaderLoader: commonLoader<GuitarFilter>(GuitarFilterModel, "shape"),
+  guitarType: commonLoader<GuitarFilter>(GuitarFilterModel, "guitarType"),
+  pickupsSet: commonLoader<GuitarFilter>(GuitarFilterModel, "pickupsSet"),
+  producer: commonLoader<GuitarFilter>(GuitarFilterModel, "producer"),
+  shape: commonLoader<GuitarFilter>(GuitarFilterModel, "shape"),
 };
 
 export type Loaders = typeof loaders;
