4db3d5d0139b3f214fdf57c5a80373a347b485db
kazu-pl <91014542+kazu-pl@users.noreply.github.com>
2023-08-31 22:25:54 +0200

    add ability to reset all items from checkout list and from wishlist (#29)

--- src/constants/COMMON_MESSAGES.ts
+++ src/constants/COMMON_MESSAGES.ts
@@ -10,9 +10,11 @@ const COMMON_MESSAGES = {
   ALREADY_IN_WISHLIST: "Ten przedmiot znajduje się już w Twojej liście życzeń",
   ADDED_TO_WISHLIST: "Pomyślnie dodano do listy życzeń",
   REMOVED_FROM_WISHLIST: "Pomyślnie usunięto z listy życzeń",
+  CLEARED_WHOLE_WISHLIST: "Usunięto wszystkie elementy z listy życzeń",
   ADDED_TO_CHECKOUT: "Pomyślnie dodano do koszyka",
   REMOVED_FROM_CHECKOUT: "Pomyślnie usunięto z koszyka",
   ITEM_DOES_NOT_EXIST_ON_CHECKOUT: "Tego produktu nie było już w koszyku",
+  CLEARED_WHOLE_CHECKOUT: "Usunięto wszystkie elementy z koszyka",
 };
 
 export default COMMON_MESSAGES;
